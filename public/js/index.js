23a91feab0adea908b122ff7bc0a1b80abbcb100
tom <tomek7sa@gmail.com>
2018-07-14 19:12:58 +0200

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on("connect", function() {
   console.log("Connected to server.");
-
-  socket.emit("createMessage", {
-    from: "Andrew",
-    text: "Yup, this works for me"
-  });
 });
 socket.on("disconnect", function() {
   console.log("Disconneced from server.");
