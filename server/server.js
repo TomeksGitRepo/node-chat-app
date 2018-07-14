0d13e71649b3b3edd33f063ed62f13e94b2913f1
tom <tomek7sa@gmail.com>
2018-07-14 19:27:10 +0200

    Greet new User and alert others

--- server/server.js
+++ server/server.js
@@ -14,21 +14,37 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected");
 
- socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', {
-        from: message.from,
-        text: message.text,
-        createdAt: new Date().getTime()
-    })
- });
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcom on server",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+  
+  socket.on("createMessage", message => {
+    console.log("createMessage", message);
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // })
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected.");
   });
 });
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
